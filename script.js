164ee69e09fde84a89fa47c41953c14b1922c845
Deanjer <220509@student.glu.nl>
2022-12-18 19:04:18 +0100

    v1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // title
 const title = document.getElementById("title")
-var titletop = "test";
-title.style.cssText += " color: black; font-size: 150px; font-weight: bold; text-align: center;";
+var titletop = "Workshop 1";
+title.style.cssText += " color: black; font-size: 150px; font-weight: bold; text-align: center ; font-family: 'Roboto', sans-serif;";
 title.innerHTML += titletop;
 
 
@@ -10,9 +10,12 @@ title.innerHTML += titletop;
 
 // container
 const container = document.getElementById("container");
-container.style.cssText += " width: 50%; margin: 0 auto; height: auto; background-color: #DBDBDB; text-align: center ";
+container.style.cssText += " width: 50%; margin: 0 auto; height: auto; background-color: #DBDBDB;  ";
 
+// center-container
 
+const center_container = document.getElementById("center_container")
+center_container.style.cssText += "width: 80%; margin-right: 80px ";
 
 // Naam begin
 // var volledigenaam = voornaam + tussenvoegsel + achternaam;
@@ -22,7 +25,7 @@ var achternaam = "Blok";
 
 const volledigenaam = document.getElementById("volledigenaam");
 // volledigenaam.style.background = '#DBDBDB'
-volledigenaam.style.cssText += "display: block; color: #529EA7; ";
+volledigenaam.style.cssText += "display: block; color: #529EA7; font-size: 30px ;font-family: 'Roboto', sans-serif; ";
 volledigenaam.innerHTML = voornaam;
 volledigenaam.innerHTML += " ";
 volledigenaam.innerHTML += tussenvoegsel;
@@ -31,10 +34,10 @@ volledigenaam.innerHTML += achternaam;
 
 
 // Naam eind
-
-
-
-
+// adres gegevens
+const adresgegevens = document.getElementById("adresgegevens")
+adresgegevens.innerHTML = "Kon. Wilhelminalaan 7, 3527 LA Utrecht";
+adresgegevens.style.cssText += "display: block; color: #529EA7; font-size: 30px ;font-family: 'Roboto', sans-serif;";
 
 
 
